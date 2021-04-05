f36b5eb715c427af9fc3cb8d245771c598dd1215
g <guidomaurocurcio@gmail.com>
2021-04-05 15:14:22 -0300

    implementing flatten from std

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,29 @@ fn something() {
     for v in vs.iter() {
         // This borrows vs and we get references into `v`. So it does not work with the preceeding call to naive owned vs.
     }
+    for v in &vs {
+        // This borrows vs and we get references into `v`. So it does not work with the preceeding call to naive owned vs.
+        // equivalent to above; being explicit.
+    }
+}
+
+pub fn flatten<I>(iter: I) -> Flatten<I> {
+    Flatten::new(iter)
+}
+
+pub struct Flatten<I> {
+    outer: I,
+}
+
+impl<I> Flatten<I> {
+    fn new(iter: I) -> Self {
+        Self { outer: iter }
+    }
+}
+
+impl<I> Iterator for Flatten<I> {
+    type Item = I;
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
